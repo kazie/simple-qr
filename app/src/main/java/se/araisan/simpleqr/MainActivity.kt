cb522970f94d57b91ea57789e2a9b60f2724d46a
Petter Salminen <petter.salminen@r2m.se>
2023-05-11 14:44:09 +0200

    Update, seems to work except permanent save

--- app/src/main/java/se/araisan/simpleqr/MainActivity.kt
+++ app/src/main/java/se/araisan/simpleqr/MainActivity.kt
@@ -4,31 +4,37 @@ import android.content.res.Configuration
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.burnoo.compose.rememberpreference.rememberStringPreference
 import io.github.g0dkar.qrcode.QRCode
-import kotlinx.coroutines.*
+import kotlinx.parcelize.Parcelize
 import se.araisan.simpleqr.ui.theme.SimpleQrTheme
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +45,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Parcelize
+data class Email(var value: String) : Parcelable
+
 @Preview(showBackground = false)
 @Composable
 fun SimpleImage(email: String = "test+hest@example.com") {
     val useMe = when (email.contains('@')) {
-        true -> email
+        true -> email.trimEnd()
         false -> "unknown@email.com"
     }
     val splitted = useMe.split('@')
@@ -66,7 +75,13 @@ fun CenterText(text: String, onNameChange: (String) -> Unit) {
     OutlinedTextField(
         value = text,
         onValueChange = { onNameChange(it) },
-        label = { Text("Insert email address") }
+        label = { Text("Insert email address") },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            autoCorrect = true,
+            imeAction = ImeAction.Done,
+        ),
     )
 }
 
@@ -82,13 +97,15 @@ fun DefaultPreview() {
             color = MaterialTheme.colors.background
         ) {
             SimpleQrTheme {
+
                 // This is sadly slow, but I could not really find a good way to debounce the save action...
-                var email by rememberStringPreference(
+                var email by rememberSaveable { mutableStateOf(Email("email@domain.com")) }
+                /*var email by rememberStringPreference(
                     keyName = "email-input",
                     // Both initial and default makes non null
                     initialValue = "test@example.com",
-                    defaultValue = "test@example.com"
-                )
+                    defaultValue = ""
+                )*/
                 Box(
                     contentAlignment = Alignment.TopCenter, // you apply alignment to all children
                     modifier = Modifier
@@ -101,9 +118,9 @@ fun DefaultPreview() {
                             modifier = Modifier
                                 .height(150.dp)
                         )
-                        SimpleImage(email)
+                        SimpleImage(email.value)
                         Spacer(modifier = Modifier.height(20.dp))
-                        CenterText(email, onNameChange = { email = it })
+                        CenterText(email.value, onNameChange = { email = Email(value = it) })
                     }
                 }
             }
